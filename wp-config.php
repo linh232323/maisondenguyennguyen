b28c684847237f551e0ef7f3fa85bb03752c4d76
Linh Ho <linh232324@gmail.com>
2014-10-12 12:14:21 +0700

    change config , add WP site URL

--- wp-config.php
+++ wp-config.php
@@ -33,6 +33,10 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+
+define('WP_HOME','http://maisondenguyennguyen.com:81');
+define('WP_SITEURL','http://maisondenguyennguyen.com:81');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
