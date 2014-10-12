5d804bc4a1c0fe7fe8860d7be944174b20b04b5c
Linh Ho <linh232324@gmail.com>
2014-10-12 11:04:37 +0700

    change config

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wooshop');
+define('DB_NAME', 'admin_maison');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'admin_maison');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '123abc!@#');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '103.27.61.14');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
