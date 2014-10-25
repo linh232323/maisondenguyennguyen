2e0870b960ca332486eaacf9f9b5efa64565a073
Linh Ho <linh232324@gmail.com>
2014-10-25 14:21:22 +0700

    change style + add menu

--- wp-content/themes/mystile/includes/theme-functions.php
+++ wp-content/themes/mystile/includes/theme-functions.php
@@ -34,6 +34,9 @@ if ( function_exists( 'wp_nav_menu') ) {
 	add_theme_support( 'nav-menus' );
 	register_nav_menus( array( 'primary-menu' => __( 'Primary Menu', 'woothemes' ) ) );
 	register_nav_menus( array( 'top-menu' => __( 'Top Menu', 'woothemes' ) ) );
+        register_nav_menus( array( 'top-left-menu' => __( 'Top Menu Left', 'woothemes' ) ) );
+        register_nav_menus( array( 'top-right-menu' => __( 'Top Menu Right', 'woothemes' ) ) );
+        register_nav_menus( array( 'foot-menu' => __( 'Top Menu', 'woothemes' ) ) );
 }
 
 /*-----------------------------------------------------------------------------------*/
