84ed570a193738a2241b6276b65ff57629e90a1c
Hien <thetukiet@gmail.com>
2014-11-24 18:42:23 +0700

    add gateway payment method

--- wp-content/plugins/woocommerce/includes/gateways/paypal/class-wc-gateway-paypal.php
+++ wp-content/plugins/woocommerce/includes/gateways/paypal/class-wc-gateway-paypal.php
@@ -152,11 +152,7 @@ class WC_Gateway_Paypal extends WC_Payment_Gateway {
 			$icon_html = '<img src="' . esc_attr( apply_filters( 'woocommerce_paypal_icon', $icon ) ) . '" alt="PayPal Acceptance Mark" />';
 		}
 
-		if ( $link ) {
-			$what_is_paypal = sprintf( '<a href="%1$s" class="about_paypal" onclick="javascript:window.open(\'%1$s\',\'WIPaypal\',\'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=1060, height=700\'); return false;" title="' . esc_attr__( 'What is PayPal?', 'woocommerce' ) . '">' . esc_attr__( 'What is PayPal?', 'woocommerce' ) . '</a>', esc_url( $link ) );
-		} else {
-			$what_is_paypal = '';
-		}
+
 
 		return apply_filters( 'woocommerce_gateway_icon', $icon_html . $what_is_paypal, $this->id );
 	}
--- wp-content/plugins/woocommerce/includes/wc-template-hooks.php
+++ wp-content/plugins/woocommerce/includes/wc-template-hooks.php
@@ -128,9 +128,9 @@ add_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_rela
  * @see woocommerce_template_single_sharing()
  */
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 10 );
+add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 20 );
+add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 20 );
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 50 );
 
